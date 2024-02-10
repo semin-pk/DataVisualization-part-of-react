371c71327ab85412c039d0247af5370e99b6e739
semin-pk <tpals11149@gmail.com>
2024-02-11 00:41:20 +0900

    '선택한항목별데이터양개수'

--- src/App.js
+++ src/App.js
@@ -13,17 +13,21 @@ class App extends React.Component {
     //ToDo에게 item이라는 이름으로 데이터를 전달
     //3개의 객체를 가진 배열 생성
     this.state = {
-      items:[]
+      items:[],
+      items_all : []
     }
   }
     //화면이 보여질 때 마다 호출되는 수명주기 함수
   componentDidMount(){
     //ajax 요청 객체 생성
     let request = new XMLHttpRequest()
+    
     //요청 준비
     request.open('GET', 'http://127.0.0.1:8000/list');
+    
     //요청
     request.send('');
+    
     //응답처리
     request.addEventListener('load', () =>{
       //json 데이터 출력
@@ -33,54 +37,71 @@ class App extends React.Component {
       let data = JSON.parse(request.responseText);
       console.log(data)
       //서버에서 받아온 데이터를 state에 저장
-      this.setState({items : data.list})
+      this.setState({items: data.list})
     })
-  }
 
-  add = (item) => {
-    //react는 props는 수정할 수 없고
-    //state는 수정이 가능하지만 원본은 직접 수정하는 것은 안되고
-    //setState 메서드를 이용해서만 수정이 가능합니다.
-    //배열에 데이터를 추가하기 위해서 state의 배열을 복사
-    const thisItems = this.state.items;
-    //item의 id와 done값을 설정 - title 만 입력하기 때문
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    //복사한 데이터에 데이터를 추가
-    thisItems.push(item)
-    //복사한 데이터를 다시 state에 적용
-    this.setState({items:thisItems})
-  } 
-  delete = (item) => {
-    //state나 props 의 데이터는 직접 편집이 안됩니다.
-    const thisItems = this.state.items;
-    //복사본에서 item를 제거
-    //filter 함수는 리턴 타입이 boolean 함수를 매개변수로 받아서
-    //리턴 결과가 true인 데이터만 모아서 배열로 리턴하는 함수.
-    const newItems = thisItems.filter((e) => e.id !== item.id)
+    let requset_all = new XMLHttpRequest()
+    requset_all.open('GET', 'http://127.0.0.1:8000/todo');
+    requset_all.send('');
 
-    //원본에 다시 복사
-    this.setState({items:newItems})
-  }
-  select = (item) => {
-    let request = new XMLHttpRequest()
-    //요청 준비
-    const url = 'http://127.0.0.1:8000/todo/?userid=VT-21700'
-    request.open('GET', url);
-    //요청
-    request.send('');
-    //응답처리
-    request.addEventListener('load', () =>{
+    requset_all.addEventListener('load', () =>{
       //json 데이터 출력
       
       //console.log(request.responseText)
       //JSON 문자열을 데이터로 변환
-      let data = JSON.parse(request.responseText);
-      console.log(data)
+      let data_all = JSON.parse(requset_all.responseText);
+      console.log(data_all)
       //서버에서 받아온 데이터를 state에 저장
-      this.setState({selectitem : data.list})
+      this.setState({items_all : data_all.list})
     })
   }
+
+  //add = (item) => {
+  //  //react는 props는 수정할 수 없고
+  //  //state는 수정이 가능하지만 원본은 직접 수정하는 것은 안되고
+  //  //setState 메서드를 이용해서만 수정이 가능합니다.
+  //  //배열에 데이터를 추가하기 위해서 state의 배열을 복사
+  //  const thisItems = this.state.items;
+  //  //item의 id와 done값을 설정 - title 만 입력하기 때문
+  //  item.id = "ID-" + thisItems.length;
+  //  item.done = false;
+  //  //복사한 데이터에 데이터를 추가
+  //  thisItems.push(item)
+  //  //복사한 데이터를 다시 state에 적용
+  //  this.setState({items:thisItems})
+  //} 
+  //delete = (item) => {
+  //  //state나 props 의 데이터는 직접 편집이 안됩니다.
+  //  const thisItems = this.state.items;
+  //  //복사본에서 item를 제거
+  //  //filter 함수는 리턴 타입이 boolean 함수를 매개변수로 받아서
+  //  //리턴 결과가 true인 데이터만 모아서 배열로 리턴하는 함수.
+  //  const newItems = thisItems.filter((e) => e.id !== item.id)
+//
+  //  //원본에 다시 복사
+  //  this.setState({items:newItems})
+  //}
+
+  //select = (item) => {
+  //  const thisItems = this.state.items;
+  //  
+  //  let request_list = new XMLHttpRequest()
+  //  //요청 준비
+  //  request_list.open('GET', 'http://127.0.0.1:8000/list');
+  //  //요청
+  //  request_list.send('');
+  //  //응답처리
+  //  request_list.addEventListener('load', () =>{
+  //    //json 데이터 출력
+  //    
+  //    //console.log(request.responseText)
+  //    //JSON 문자열을 데이터로 변환
+  //    let data_column = JSON.parse(request_list.responseText);
+  //    console.log(data_column)
+  //    //서버에서 받아온 데이터를 state에 저장
+  //    this.setState({items : data_column.list})
+  //}
+  
   render(){
     //배열을 순회하면서 출력할 내용을 생성
     //배열을 순회하면서 출력물을 만들 때는 key를 설정해주어야 함.
@@ -98,8 +119,7 @@ class App extends React.Component {
     return(
       <div className = "App">
         <Container>
-          <AddToDo add = {this.add} />
-          <ToDo item = {this.state.items} select = {this.select} />
+          <ToDo item = {this.state.items} item_all = {this.state.items_all}  />
         </Container>
       </div>
     )
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from 'react';
 import Select from "react-select"
 import {
     ListItem,
@@ -10,88 +10,72 @@ import {
 } from "@material-ui/core"
 
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined"
-//react 에서 컴포넌트 클래스를 만들기 위해서 Component로 부터 상속을 받기
-class ToDo extends React.Component{
-    //생성자
-    constructor(props) {
-        //App.js에서 넘겨준 속성들을 자신의 props에 저장
-        //this.props.item이라는 속성에 앞에서 전달한 데이터를 저장
+
+function ToDo(props) {
+    const [x_choice, setXChoice] = useState('');
+    const [y_choice, setYChoice] = useState('');
+    const handleChange = (selectedItem, axis) => {
+        if(axis === 'x') {
+            const selectedValues_x = props.item_all.map((item_all) => 
+                item_all[selectedItem.value]
+            );
+            let dict_x = {};
+            selectedValues_x.forEach((x) => {
+                if (x in dict_x){
+                    dict_x[x] += 1;
+                }
+                else{
+                    dict_x[x] = 1;
+                }
+            });
+            setXChoice(dict_x);
+        } else {
+            const selectedValues_y = props.item_all.map((item_all) =>
+                item_all[selectedItem.value]
+            );
+            let dict_y = {};
+            selectedValues_y.forEach((y) => {
+                if (y in dict_y){
+                    dict_y[y] += 1;
+                }
+                else{
+                    dict_y[y] = 1;
+                }
+            });
+            setYChoice(dict_y);
+        }       
         
-        super(props); //상위 클래스에서 넘겨준 모든 props를 현재 클래스에 저장
-        //state 생성
-        //item이라는 이름으로 props 중에서 item이라는 값을 state로 저장
-        //props는 상위 컴포넌트에서 전달한 데이터라서 읽기는 가능하지만 
-        //수정이나 삭제가 안되므로 수정이나 삭제를 하고자 하는 경우는 
-        //state로 변환
-        this.state = {item:props.item,
-            selectitem : []
-        }
-
-        //App.js에서 넘겨준 삭제 함수를 현재 클래스의 데이터로 변환
-        this.delete = this.props.delete
-
     }
-    //삭제 버튼을 누를 때 호출될 이벤트 핸들러
-    deleteEventHandler = (e) => {
-        this.delete(this.state.item)
-    }
-    selectEventHandler = (selectedItem) => {
-        this.props.select(selectedItem);
-    };
-    //화면에 출력할 내용을 리턴하는 메서드
-    render() {
-        //자주 사용하는 데이터를 짧게 사용하기 위해서 다른 변수에 대입
-        const {item, selectitem} = this.state;
-        //const option = [item.map((item, idx) =>( 
-        //    {value : {item}, label : {item}}
-        //  ))]
-        const options = this.state.item.map(item => ({ value: item, label: item }));
 
-        return(
+    //const deleteEventHandler = () => {
+    //    props.delete(props.item);
+    //}
 
-            <><Select
-                onChange = {this.selectEventHandler}
+    const options = props.item.map(item => ({ value:item, label:item}));
+    //const selects_x = props.item_all.map(item_all => (item_all.quantity));
+    //const selects_y = props.item_all.map(item_all => (item_all.sales));
+
+
+
+    return (
+        <>
+            <Select
+                onChange={(selectedItem) => handleChange(selectedItem, 'x')}
+                value={x_choice}
                 options={options}
                 placeholder={'x축'}
-                clearable={false} />
+                clearable={false} 
+            />
             <Select
-                onChange = {this.selectEventHandler}
+                onChange={(selectedItem) => handleChange(selectedItem, 'y')}
+                value={y_choice}
                 options={options}
                 placeholder={'y축'}
-                clearable={false} />
-
-            <ListItemText>
-                <InputBase
-                 inputProps = {{"aria-label":"naked"}}
-                 type = "text"
-                 id = {selectitem}
-                 name = {selectitem}
-                 value = {selectitem}
-                 multiline = {true}
-                 fullWidth = {true} />
-            </ListItemText> 
-            </>
-
-           //<ListItem>
-            //<Checkbox checked = {item.done} />
-            //<ListItemText>
-            //    <InputBase
-            //     inputProps = {{"aria-label":"naked"}}
-            //     type = "text"
-            //     id = {item.id}
-            //     name = {item.id}
-            //     value = {item}
-            //     multiline = {true}
-            //     fullWidth = {true} />
-            //</ListItemText>
-//
-           // <ListItemSecondaryAction>
-           //     <IconButton aria-label="Delete ToDo"  onClick={this.deleteEventHandler}>
-           //         <DeleteOutlined />
-           //     </IconButton>
-           // </ListItemSecondaryAction>
-           //</ListItem>
-        )
-    }
+                clearable={false}
+            />
+            <h3>{JSON.stringify(x_choice)}  {JSON.stringify(y_choice)}</h3>
+        </>
+    );
 }
-export default ToDo
+
+export default ToDo;
